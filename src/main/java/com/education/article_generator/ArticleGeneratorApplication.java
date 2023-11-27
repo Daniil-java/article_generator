30c59724637f1ed4824292c187b9702ce7f2e2a9
ПК <saponembigas@gmail.com>
2023-11-27 15:07:51 +0700

    feature/et-1-3

---
+++ src/main/java/com/education/article_generator/ArticleGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.education.article_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/education/article_generator/ArticleGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.education.article_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
