8ddc690cf131169401473bf0db8dff4eefba3813
ПК <saponembigas@gmail.com>
2023-12-04 16:20:30 +0700

    fix(et-4): endpoints rename

--- src/main/java/com/education/articlegenerator/controllers/ArticleTopicController.java
+++ src/main/java/com/education/articlegenerator/controllers/ArticleTopicController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/topic")
+@RequestMapping("/api/v1/articletopic")
 @RequiredArgsConstructor
 public class ArticleTopicController {
     private final ArticleTopicService articleTopicService;
