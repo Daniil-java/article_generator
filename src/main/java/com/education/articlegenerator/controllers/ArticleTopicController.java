612bd02c09d47aa331bc695cf54e6759f1835c5f
ПК <saponembigas@gmail.com>
2024-01-02 19:33:31 +0700

    fix(et-11): Подключение Swagger

--- src/main/java/com/education/articlegenerator/controllers/ArticleTopicController.java
+++ src/main/java/com/education/articlegenerator/controllers/ArticleTopicController.java
@@ -1,7 +1,15 @@
 package com.education.articlegenerator.controllers;
 
+import com.education.articlegenerator.dtos.ErrorResponse;
+import com.education.articlegenerator.entities.Article;
 import com.education.articlegenerator.entities.ArticleTopic;
 import com.education.articlegenerator.services.ArticleTopicService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,14 +18,37 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/articletopic")
 @RequiredArgsConstructor
+@Tag(name = "Заголовки статей", description = "Методы работы с заголовками")
 public class ArticleTopicController {
     private final ArticleTopicService articleTopicService;
 
+    @Operation(
+            summary = "Запрос на получение всех заголовков",
+            responses = {
+                    @ApiResponse(
+                            description = "Успешный ответ",
+                            content = @Content(array = @ArraySchema(schema = @Schema(implementation = ArticleTopic.class)))
+                    )
+            }
+    )
     @GetMapping("/all")
     public List<ArticleTopic> getAll() {
         return articleTopicService.getAll();
     }
 
+    @Operation(
+            summary = "Запрос на получение всех заголовков по Id запроса",
+            responses = {
+                    @ApiResponse(
+                            description = "Успешный ответ", responseCode = "200",
+                            content = @Content(array = @ArraySchema(schema = @Schema(implementation = ArticleTopic.class)))
+                    ),
+                    @ApiResponse(
+                            description = "Провальный ответ", responseCode = "400",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+                    )
+            }
+    )
     @GetMapping
     public List<ArticleTopic> getTopics(@RequestParam Long requestId) {
         return articleTopicService.getTopicsByRequestId(requestId);
--- src/main/java/com/education/articlegenerator/dtos/ArticleDto.java
+++ src/main/java/com/education/articlegenerator/dtos/ArticleDto.java
@@ -1,10 +1,13 @@
 package com.education.articlegenerator.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 @Data
 @Accessors(chain = true)
+@Schema(description = "Модель статьи")
 public class ArticleDto {
+    @Schema(description = "Текст статьи")
     private String articleBody;
 }
